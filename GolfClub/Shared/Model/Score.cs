e9b3a5d5a847daaeda2f2a9aafe1348a0b88e567
PetraHrabakova <petrahrab3@gmail.com>
2023-07-03 12:45:56 +0200

    Changes to 2 model classes

--- GolfClub/Shared/Model/Score.cs
+++ GolfClub/Shared/Model/Score.cs
@@ -4,6 +4,5 @@ public class Score
 {
     public int Id { get; set; }
     public int PlayerId { get; set; }
-    // Example {"1": "2"} - key = hole number, value = number of strokes
-    public Dictionary<int, int> HolesAndStrokes { get; set; }
+    public ICollection<int> Strokes { get; set; } 
 }
