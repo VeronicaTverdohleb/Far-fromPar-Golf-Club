7f4e0f31732861ea211b9f3648d5ee8addf9dca9
PetraHrabakova <petrahrab3@gmail.com>
2023-07-04 18:05:26 +0200

    forgotten DAOs, sorry

---
+++ GolfClub/DataAccess/DAOs/HoleDao.cs
@@ -0,0 +1,24 @@
+﻿using Application.DaoInterfaces;
+using Shared.Model;
+
+namespace DataAccess.DAOs;
+
+/// <summary>
+/// Data Access Object responsible for interacting with the database
+/// Used in HoleLogic
+/// </summary>
+public class HoleDao : IHoleDao
+{
+    private readonly DataContext context;
+
+    public HoleDao(DataContext context)
+    {
+        this.context = context;
+    }
+    public Task<IEnumerable<Hole>> GetAllHolesAsync()
+    {
+        IEnumerable<Hole> result = context.Holes.AsEnumerable();
+
+        return Task.FromResult(result);
+    }
+}
---
+++ GolfClub/DataAccess/DAOs/ScoreDao.cs
@@ -0,0 +1,31 @@
+﻿using Application.DaoInterfaces;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Shared.Model;
+
+namespace DataAccess.DAOs;
+
+/// <summary>
+/// Data Access Object responsible for interacting with the database
+/// Used in ScoreLogic
+/// </summary>
+public class ScoreDao : IScoreDao
+{
+    private readonly DataContext context;
+
+    public ScoreDao(DataContext context)
+    {
+        this.context = context;
+    }
+    
+    /// <summary>
+    /// Method that adds a new Score to the database
+    /// </summary>
+    /// <param name="score"></param>
+    /// <returns></returns>
+    public async Task<Score> CreateAsync(Score score)
+    {
+        EntityEntry<Score> added = await context.Scores.AddAsync(score);
+        await context.SaveChangesAsync();
+        return added.Entity;
+    }
+}
