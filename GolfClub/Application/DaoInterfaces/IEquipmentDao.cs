9ac5ef01256a06ab7a297ae39744d5dadae046f7
VeronicaTverdohleb <101176218+VeronicaTverdohleb@users.noreply.github.com>
2023-07-07 00:50:12 +0200

    trying to push equipment again

--- GolfClub/Application/DaoInterfaces/IEquipmentDao.cs
+++ GolfClub/Application/DaoInterfaces/IEquipmentDao.cs
@@ -1,6 +1,13 @@
-﻿namespace Application.DaoInterfaces;
+﻿using Shared.Model;
+
+namespace Application.DaoInterfaces;
 
 public interface IEquipmentDao
 {
-    
+    Task<Equipment> CreateEquipmentAsync(Equipment equipment);
+    Task UpdateEquipmentAsync(Equipment equipment);
+    Task<IEnumerable<Equipment>> GetEquipmentsAsync();
+    Task<Equipment?> GetEquipmentByNameAsync(string name);
+    Task<Equipment?> GetEquipmentByIdAsync(int id);
+    Task DeleteEquipmentAsync(IEnumerable<string> name);
 }
--- GolfClub/Shared/Dtos/EquipmentDto/EquipmentBasicDto.cs
+++ GolfClub/Shared/Dtos/EquipmentDto/EquipmentBasicDto.cs
@@ -2,5 +2,25 @@
 
 public class EquipmentBasicDto
 {
+  
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Amount { get; set; }
     
+    public EquipmentBasicDto(int id, string name, int amount)
+    {
+        Id = id;
+        Name = name;
+        Amount = amount;
+    }
+    
+    public EquipmentBasicDto(string name, int amount)
+    {
+        
+        Name = name;
+        Amount = amount;
+    }
+    public EquipmentBasicDto(){}
+
+
 }
