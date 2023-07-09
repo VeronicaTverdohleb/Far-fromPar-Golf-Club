431056716acba6900b59bbb89e8f5a57f31134ef
PetraHrabakova <petrahrab3@gmail.com>
2023-07-09 14:48:55 +0200

    finalized page CreateGame

--- GolfClub/Shared/Model/Tournament.cs
+++ GolfClub/Shared/Model/Tournament.cs
@@ -1,10 +1,14 @@
-﻿namespace Shared.Model;
+﻿using System.Text.Json.Serialization;
+
+namespace Shared.Model;
 
 public class Tournament
 {
     public string Name { get; set; }
     public DateOnly StartDate { get; set; }
     public DateOnly EndDate { get; set; }
+    
+    [JsonIgnore]
     public ICollection<Game>? Games { get; set; }
 
     public Tournament(string name, DateOnly startDate, DateOnly endDate)
